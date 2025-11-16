6c9921dcd8b6bf63d0341052e19f6a3a467a231b
MadhalasaSJ <112635902+MadhalasaSJ@users.noreply.github.com>
2025-11-16 23:21:03 +0530

    Update import path for analyze_resume function

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from utils.back4app_mcp import save_evaluation
 
 # Try to import the analysis module; catch initialization errors
 try:
-    from utils.openai_ats import analyze_resume, ACTIVE_PROVIDER, ACTIVE_MODEL
+    from utils.model_ats import analyze_resume, ACTIVE_PROVIDER, ACTIVE_MODEL
     ANALYSIS_READY = True
     ANALYSIS_ERROR = None
 except Exception as e:
